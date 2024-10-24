a854a9dd23c5770605a7d29921fb68b4ed94d477
Aster <galaster@foxmail.com>
2024-10-24 20:10:16 +0800

    Initialize the hook structure

--- projects/sub_projects/src/main.rs
+++ projects/sub_projects/src/main.rs
@@ -1,14 +1,6 @@
-use dll_syringe::{Syringe, process::OwnedProcess};
-use sysinfo::{PidExt, ProcessExt, SystemExt};
+use dll_syringe::{process::OwnedProcess, Syringe};
 
 fn main() {
-    // let mut system = sysinfo::System::new();
-    // system.refresh_processes();
-    // for (id, p) in system.processes() {
-    //     if id.as_u32().eq(&27808) {
-    //         println!("{}:{}", id, p.name());
-    //     }
-    // }
     let target_process = OwnedProcess::find_first_by_name("gamemd").unwrap();
     let syringe = Syringe::for_process(target_process);
     let here = std::env::current_exe().unwrap();
