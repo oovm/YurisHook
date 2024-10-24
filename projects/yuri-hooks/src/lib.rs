6c22daaa9bbaef8691fe276c68a7763aa33e4e8e
Aster <galaster@foxmail.com>
2024-10-24 23:22:09 +0800

    Try to find the game process

--- projects/yuri-hooks/src/lib.rs
+++ projects/yuri-hooks/src/lib.rs
@@ -11,50 +11,7 @@ mod errors;
 // use std::ffi::{c_char, CString};
 pub use crate::errors::{Result, YuriError, YuriErrorKind};
 
-#[repr(C)]
-#[derive(Copy, Clone)]
-pub struct GroundType {
-    // Terrain speed multipliers.
-    cost: [f32; 8],
-    // Can build on this terrain?
-    buildable: i32,
-}
-
-impl GroundType {
-    pub fn get_land_type_from_name() -> bool {
-        unsafe {
-            let f: extern "fastcall" fn() -> bool = unsafe { std::mem::transmute(0x4E12D0 as *const ()) };
-            f()
-        }
-    }
-}
-
-//     use detour::RawDetour;
-//     use std::mem;
-//
-//     fn add5(val: i32) -> i32 {
-//         val + 5
-//     }
-//
-//     fn add10(val: i32) -> i32 {
-//         val + 10
-//     }
-//
-//     let mut hook = unsafe { RawDetour::new(add5 as *const (), add10 as *const ())? };
-//
-//     assert_eq!(add5(5), 10);
-//     assert_eq!(hook.is_enabled(), false);
-//
-//     unsafe { hook.enable()? };
-//     assert!(hook.is_enabled());
-//
-//     let original: fn(i32) -> i32 = unsafe { mem::transmute(hook.trampoline()) };
-//
-//     assert_eq!(add5(5), 15);
-//     assert_eq!(original(5), 10);
-//
-//     unsafe { hook.disable()? };
-//     assert_eq!(add5(5), 10);
+mod map_hooks;
 
 #[used]
 #[doc(hidden)]
---
+++ projects/yuri-hooks/src/map_hooks/mod.rs
@@ -0,0 +1,17 @@
+#[repr(C)]
+#[derive(Copy, Clone)]
+pub struct GroundType {
+    // Terrain speed multipliers.
+    cost: [f32; 8],
+    // Can build on this terrain?
+    buildable: i32,
+}
+
+impl GroundType {
+    pub fn get_land_type_from_name() -> bool {
+        unsafe {
+            let f: extern "fastcall" fn() -> bool = unsafe { std::mem::transmute(0x4E12D0 as *const ()) };
+            f()
+        }
+    }
+}
